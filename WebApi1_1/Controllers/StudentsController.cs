f1bd7e0085577b522672f5cea71c945375c394a8
Samikshya <write2samikshyakar@gmail.com>
2021-10-30 13:38:39 +0530

    Add project files.

---
+++ WebApi1_1/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi1_1.Controllers
+{
+    public class StudentsController: ApiController
+    {
+
+
+
+
+
+    }
+}
---
+++ WebApi1_1/Controllers/ValuesConroller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi1_1.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class ValuesConroller:ApiController
+    {
+        List<string> strings = new List<string>()
+        {
+             "value1", "Value2","Value3"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return strings;
+        }
+
+        public string Get(int id )
+        {
+            return strings[id];
+        }
+
+    }
+}
